4b306116994a5cfd30bed1dfdbbf561490e0abfb
nicolalesi <109904165+nicolalesi@users.noreply.github.com>
2022-12-28 08:54:57 +0100

    Aggiustamenti vari

--- progettoTec/script/fontPage.js
+++ progettoTec/script/fontPage.js
@@ -2,15 +2,15 @@ head = "<div class='card text-center' id='head'>";
 head += "<div class='card-header'>";
 head += "<h3> Animal house</h3>";
 head += "<ul class='nav nav-tabs card-header-tabs' id='navbar'>";
-head += "<li class='nav-item'><a class='nav-link active' href='home.html'>Home</a></li>";  
-head += "<li class='nav-item'><a class='nav-link' href='scopri.html'>Scopri nuovi animali</a></li>";    
-head += "<li class='nav-item'><a class='nav-link' href='quiz.html'>Quiz</a></li>";        
-head += "<li class='nav-item'><a class='nav-link' href='impiccato.html'>Impiccato</a></li>";        
-head += "<li class='nav-item'><a class='nav-link' href='curiosita.html'>Curiosit&agrave;</a></li>";        
-head += "<li class='nav-item'><a class='nav-link' href='funFacts.html' tabindex='-1' aria-disabled='true'>Funny and cutie images</a></li>";        
-head += "<li class='nav-item' id='login'><a class='nav-link' href='login.html' tabindex='-1' aria-disabled='true' >Login</a></li>";        
-head += "<li class='nav-item' id='areaPersonale'><a class='nav-link' href='areaPersonale.html' tabindex='-1' aria-disabled='true'>Area personale</a></li>";        
-head += "<li class='nav-item' id='bacheca'><a class='nav-link' href='bacheca.html' tabindex='-1' aria-disabled='true'>Bacheca</a></li>";        
+head += "<li class='nav-item'><a class='nav-link' href='home.html' id='home'>Home</a></li>";  
+head += "<li class='nav-item'><a class='nav-link' href='scopri.html' id='scopri'>Scopri nuovi animali</a></li>";    
+head += "<li class='nav-item'><a class='nav-link' href='quiz.html' id='quiz'>Quiz</a></li>";        
+head += "<li class='nav-item'><a class='nav-link' href='impiccato.html' id='impiccato'>Impiccato</a></li>";        
+head += "<li class='nav-item'><a class='nav-link' href='curiosita.html' id='curiosit' >Curiosit&agrave;</a></li>";        
+head += "<li class='nav-item'><a class='nav-link' href='funFacts.html' tabindex='-1' aria-disabled='true' id='funFacts'>Funny and cutie images</a></li>";        
+head += "<li class='nav-item' id='login'><a class='nav-link' href='login.html' tabindex='-1' aria-disabled='true' id='login' >Login</a></li>";        
+head += "<li class='nav-item' id='areaPersonale'><a class='nav-link' href='areaPersonale.html' tabindex='-1' aria-disabled='true' id='personal'>Area personale</a></li>";        
+head += "<li class='nav-item' id='bacheca'><a class='nav-link' href='bacheca.html' tabindex='-1' aria-disabled='true' id='bacheca'>Bacheca</a></li>";        
 head += "</ul></div></div>";  
 
 footer = "<footer class='bg-light text-center text-lg-start' >"
